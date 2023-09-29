639570fb2a59b678b7c69d22e30645a8084ce33c
alexeymametyev <mametevalex@gmail.com>
2023-09-29 20:36:32 +0300

    fixed packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ygpt',
-    packages=find_packages(include=['ygpt', 'ygpt.*']),
+    packages=find_packages(include=['ygpt', 'ygpt.*', 'ygpt.utils']),
     version='0.1.0',
     description='Yandendex GPT2 api wrapper',
     author='t.me/freQuensy23',
